4f7d2126479b17c549b7750d81f934db7e3dbbce
Smith Patel <deep.smith100@gmail.com>
2025-11-09 23:23:47 -0500

    Minor changes

--- src/app/api/profile/[username]/route.ts
+++ src/app/api/profile/[username]/route.ts
@@ -3,9 +3,9 @@ import { NextResponse } from "next/server";
 
 export async function GET(
   req: Request,
-  { params }: { params: { username: string }}
+  { params }: { params: Promise<{ username: string }> }
 ) {
-  const { username } = params;
+  const { username } = await params;
 
   try {
     const profile = await prisma.profile.findUnique({
